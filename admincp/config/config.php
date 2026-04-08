783915ef7887081b6279257f44a4001e12251b3f
DoHoangNam-23810310303 <DoHoangNam-23810310303@users.noreply.github.com>
2026-04-08 10:48:37 +0700

    Remove admincp folder from progress scope

--- admincp/config/config.php
+++
@@ -1,9 +0,0 @@
-    <?php
-        $mysqli = new mysqli("localhost","root","","webbh1");
-
-        // Check connection
-        if ($mysqli -> connect_errno) {
-        echo "Kết nối lỗi: " . $mysqli -> connect_error;
-        exit();
-        }
-    ?>
